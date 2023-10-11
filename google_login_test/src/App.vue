639937ba91308e0fdd8a77324a377c28dfca27a6
SizoDevelopment <98975535+SizoDevelopment@users.noreply.github.com>
2023-10-11 14:19:26 +0200

    Google OAuth connection to retrieve access_token

--- google_login_test/src/App.vue
+++ google_login_test/src/App.vue
@@ -1,24 +1,28 @@
 
 <template>
-  <button @click="login">Login Using Google</button>
+  <button @click="oauthSignIn">Login Using Google</button>
 </template>
 
 <script setup>
 import axios from 'axios';
 import { googleTokenLogin } from "vue3-google-login"
 
-const login = () => {
-  googleTokenLogin().then((response) => {
-    console.log("Handle the response", response);
+const apiUrl = 'https://localhost:7174/Test/test';
+var fragmentString = location.hash.substring(1);
 
-    const apiUrl = 'https://localhost:7174/Test/test';
-
-    // Send the access token in the request body as a JSON object
-    axios.post(apiUrl, { access_token: response.access_token },{
+var params = {};
+  var regex = /([^&=]+)=([^&]*)/g, m;
+  while (m = regex.exec(fragmentString)) {
+    params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+  }
+  if (Object.keys(params).length > 0) {
+    localStorage.setItem('oauth2-test-params', JSON.stringify(params) );
+    var params = JSON.parse(localStorage.getItem('oauth2-test-params'));
+    if (params && params['access_token']) {
+      axios.post(apiUrl, { access_token: params['access_token'] },{
     headers : {
     'Content-Type': 'application/json'
-  }})
-      .then(response => {
+  }}) .then(response => {
         // Handle the API response
         console.log(response.data);
       })
@@ -26,6 +30,37 @@ const login = () => {
         // Handle errors
         console.error(error);
       });
-  });
-};
+    }
+  }
+
+function oauthSignIn() {
+  // Google's OAuth 2.0 endpoint for requesting an access token
+  var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+
+  // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+  var form = document.createElement('form');
+  form.setAttribute('method', 'GET'); // Send as a GET request.
+  form.setAttribute('action', oauth2Endpoint);
+
+  // Parameters to pass to OAuth 2.0 endpoint.
+  var params = {'client_id': '413309332388-asggfrgcbh5r1vul4eqv0l3t4o0dsq5h.apps.googleusercontent.com',
+                'redirect_uri': 'http://localhost:3000/auth/callback',
+                'response_type': 'token',
+                'scope': 'https://www.googleapis.com/auth/cloud-platform https://www.googleapis.com/auth/service.management.readonly',
+                'include_granted_scopes': 'true',
+                'state': 'pass-through value'};
+
+  // Add form parameters as hidden input values.
+  for (var p in params) {
+    var input = document.createElement('input');
+    input.setAttribute('type', 'hidden');
+    input.setAttribute('name', p);
+    input.setAttribute('value', params[p]);
+    form.appendChild(input);
+  }
+
+  // Add form to page and submit it to open the OAuth 2.0 endpoint.
+  document.body.appendChild(form);
+  form.submit();
+}
 </script>
--- google_login_test/src/main.js
+++ google_login_test/src/main.js
@@ -5,7 +5,7 @@ import vue3GoogleLogin from 'vue3-google-login'
 const app = createApp(App)
 
 app.use(vue3GoogleLogin, {
-  clientId: '776740360435-3ro03vug02rv3gp61o1td2g1h5jvdcfk.apps.googleusercontent.com'
+  clientId: '413309332388-asggfrgcbh5r1vul4eqv0l3t4o0dsq5h.apps.googleusercontent.com',
 })
 
 app.mount('#app')

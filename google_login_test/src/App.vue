607332dfd7370755998f9f82ba69d523e3669b10
MeesVaessen <507244@student.fontys.nl>
2023-10-11 09:29:36 +0200

    test

--- google_login_test/src/App.vue
+++ google_login_test/src/App.vue
@@ -9,18 +9,23 @@ import { googleTokenLogin } from "vue3-google-login"
 
 const login = () => {
   googleTokenLogin().then((response) => {
-    console.log("Handle the response", response)
+    console.log("Handle the response", response);
+
     const apiUrl = 'https://localhost:7174/Test/test';
 
-    axios.post(apiUrl, response)
-  .then(response => {
-    // Handle the API response
-    console.log(response.data);
-  })
-  .catch(error => {
-    // Handle errors
-    console.error(error);
+    // Send the access token in the request body as a JSON object
+    axios.post(apiUrl, { access_token: response.access_token },{
+    headers : {
+    'Content-Type': 'application/json'
+  }})
+      .then(response => {
+        // Handle the API response
+        console.log(response.data);
+      })
+      .catch(error => {
+        // Handle errors
+        console.error(error);
+      });
   });
-  })
-}
+};
 </script>
